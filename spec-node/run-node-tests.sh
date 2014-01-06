aea1ff06a7a14d2249ee6c1dc5a8b85db6b725b0
Justin Searls <searls@gmail.com>
2014-01-06 15:40:15 -0500

    make travis happy

--- spec-node/run-node-tests.sh
+++ spec-node/run-node-tests.sh
@@ -5,4 +5,3 @@ rm -rf node_modules/grunt-jasmine-bundle/node_modules/jasmine-stealth
 ln -s "$(pwd)/.." node_modules/grunt-jasmine-bundle/node_modules/jasmine-stealth
 cp ../spec/*.coffee spec
 grunt spec:unit
-rm spec/*.coffee
