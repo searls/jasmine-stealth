7417a7463b62c791a062269106f2b6f2bdeb14a4
Justin Searls <searls@gmail.com>
2014-01-06 15:40:48 -0500

    travis

--- spec-node/run-node-tests.sh
+++ spec-node/run-node-tests.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+npm install -g grunt-cli
 npm install
 rm -rf node_modules/grunt-jasmine-bundle/node_modules/jasmine-stealth
 ln -s "$(pwd)/.." node_modules/grunt-jasmine-bundle/node_modules/jasmine-stealth
 cp ../spec/*.coffee spec
 grunt spec:unit
+rm -rf spec/*.coffee
